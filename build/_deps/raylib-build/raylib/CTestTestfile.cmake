e43653df1b35e4f0e6e54e53be2ab36fc819425b
Tania Bezancon <tania.bezancon@epitech.eu>
2023-01-23 12:04:45 +0100

    feat: Add Sprites for the menus

---
+++ build/_deps/raylib-build/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/taniabzn/Documents/Epitech/Tek3/R-type/B-CPP-500-LYN-5-2-rtype-tania.bezancon/build/_deps/raylib-src/src
+# Build directory: /Users/taniabzn/Documents/Epitech/Tek3/R-type/B-CPP-500-LYN-5-2-rtype-tania.bezancon/build/_deps/raylib-build/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
